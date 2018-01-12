3bc9b7766c1147cf81ad064085949a7552dcf2f5
Daniel Testa <dantesta@Daniels-MacBook-Pro.local>
2018-01-12 01:46:45 -0500

    page scroll animation javascript

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,1 +1,36 @@
 // Single page navigation, button to scroll up and down the page
+
+
+$('a[href*="#"]')
+  // Delete blank links
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // Links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+          // returns to top in 1.5 seconds
+        }, 1500, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
